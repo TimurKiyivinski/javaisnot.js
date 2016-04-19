f1c5f0dd3b6244fbc7deb5af8c7da9d7bacc270e
Timur Kiyivinski <deathseclipse@gmail.com>
2016-04-19 18:03:18 +0800

    Test model query

--- src/com/Kiyivinski/Main.java
+++ src/com/Kiyivinski/Main.java
@@ -6,15 +6,31 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        boolean verbose = true;
+        boolean verbose = false;
 
-        Model course = new Course(verbose);
+        Course course = new Course(verbose);
+        Student student = new Student(verbose);
+        Unit unit = new Unit(verbose);
+        Semester semester = new Semester(verbose);
+        Assessment assessment = new Assessment(verbose);
+        Type type = new Type(verbose);
+        StudentUnit studentUnit = new StudentUnit(verbose);
+        StudentAssessment studentAssessment = new StudentAssessment(verbose);
+        UnitAssessment unitAsseessment = new UnitAssessment(verbose);
 
         Display myDisplay = new CLIDisplay();
 
         if (course.connect("192.168.99.100", "root", "password", "java", 3306)) {
             try {
                 myDisplay.printModel(course.all(), Course.getColumns());
+                myDisplay.printModel(student.all(), Student.getColumns());
+                myDisplay.printModel(unit.all(), Unit.getColumns());
+                myDisplay.printModel(semester.all(), Semester.getColumns());
+                myDisplay.printModel(assessment.all(), Assessment.getColumns());
+                myDisplay.printModel(type.all(), Type.getColumns());
+                myDisplay.printModel(studentUnit.all(), StudentUnit.getColumns());
+                myDisplay.printModel(studentAssessment.all(), StudentAssessment.getColumns());
+                myDisplay.printModel(unitAsseessment.all(), UnitAssessment.getColumns());
             } catch (Exception e) {
                 myDisplay.printError("SQL Error occurred");
             }
