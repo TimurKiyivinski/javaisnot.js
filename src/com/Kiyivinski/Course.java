684bf1e6937490da0d321409b79bb5e667b8635f
Timur Kiyivinski <deathseclipse@gmail.com>
2016-04-20 00:28:23 +0800

    Course create() functionality

--- src/com/Kiyivinski/Course.java
+++ src/com/Kiyivinski/Course.java
@@ -1,6 +1,8 @@
 package com.Kiyivinski;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Course extends Model {
     Course(boolean verbose) {
@@ -12,5 +14,11 @@ public class Course extends Model {
         columns.add("id");
         columns.add("name");
         return columns;
+    }
+
+    public void create(String name) throws SQLException {
+        HashMap<String, String> columns = new HashMap<>();
+        columns.put("name", name);
+        super.create(columns);
     }
 }
